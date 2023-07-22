4d8f63d5eac886e8e64fdde9080f8f97b9745b80
Jonathan Rodriguez <jonathanrodriguez981207@gmail.com>
2023-07-22 06:45:13 -0500

    Ajuste de share-buttons.component.html

--- src/app/shared/share-buttons/share-buttons.component.ts
+++ src/app/shared/share-buttons/share-buttons.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {VerbosityLevel} from "ngx-extended-pdf-viewer";
+import {NativeWindowRef, NativeWindowService} from "../../core/services/window.service";
 
 @Component({
   selector: 'ds-share-buttons',
@@ -9,10 +10,11 @@ import {VerbosityLevel} from "ngx-extended-pdf-viewer";
 export class ShareButtonsComponent implements OnInit {
 
   linkpage: string;
-  constructor() { }
+  constructor(@Inject(NativeWindowService) private _window: NativeWindowRef,
+  ) { }
 
   ngOnInit(): void {
-    this.linkpage = window.location.href;
+    this.linkpage = this._window.nativeWindow.location.href;
   }
 
 }
